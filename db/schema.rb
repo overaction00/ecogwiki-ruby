12ca8e7f3abed665764e4f1a37692b9196dfb539
JAEGEUN LEE <overaction00@gmail.com>
2014-05-02 16:58:09 +0900

    add unicorn.rb for connecting with nginx

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140501070913) do
+ActiveRecord::Schema.define(:version => 20140501075929) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -30,5 +30,18 @@ ActiveRecord::Schema.define(:version => 20140501070913) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "wikipages", :force => true do |t|
+    t.string   "itemtype"
+    t.string   "title"
+    t.string   "body"
+    t.string   "description"
+    t.integer  "revision"
+    t.string   "comment"
+    t.integer  "user_id"
+    t.integer  "modifier"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
