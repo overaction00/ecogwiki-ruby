a147d956f5056294ad7944c51581ede1eba8206e
overaction00 <overaction00@gmail.com>
2014-05-05 22:24:49 +0900

    feature : 모델 릴레이션을위한 foreign key설정

--- app/models/wikipage.rb
+++ app/models/wikipage.rb
@@ -2,6 +2,8 @@ class Wikipage < ActiveRecord::Base
   attr_accessible :title, :body, :itemtype, :description, :revision, :user_id,
                   :modifier, :acl_read, :acl_write, :link_id, :published_at, :published_to
 
+  has_many :old_wikipages
+
   def fill_in_default(user)
     self.newer_title = self.title
     self.modifier = self.user_id
@@ -10,6 +12,7 @@ class Wikipage < ActiveRecord::Base
   end
 
   def update_body(body)
+    self.old_wikipages.new
     self.body = body
     self.revision += 1
   end
