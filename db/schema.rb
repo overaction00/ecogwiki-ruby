a5771be2f5988bc2856f7291b066283202dba497
overaction00 <overaction00@gmail.com>
2014-05-05 22:19:25 +0900

    feature : revision 관리를 위한 old_wikipage 모델생성

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140505103653) do
+ActiveRecord::Schema.define(:version => 20140505131828) do
+
+  create_table "old_wikipages", :force => true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "revision"
+    t.integer  "user_id"
+    t.datetime "published_at"
+    t.string   "published_to"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
